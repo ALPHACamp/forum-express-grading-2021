8f759af790b607babf2f222ce50dc09fead397d3
rayray1010 <da866810@gmail.com>
2021-11-08 22:17:26 +0800

    feat:Confirm user password and unique email

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const db = require('./models/index')
+const db = require('./models')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 app.engine('hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
+
+require('./routes')(app)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-require('./routes')(app)
-
 module.exports = app
---
+++ controllers/userController.js
@@ -0,0 +1,43 @@
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+
+const userController = {
+  signUpPage: (req, res) => {
+    return res.render('signup')
+  },
+
+  signUp: async (req, res) => {
+    if (req.body.passwordCheck !== req.body.password) {
+      req.flash('error_messages', '兩次密碼輸入不相同！')
+      return res.redirect('/signup')
+    } else {
+      try {
+        const user = await User.findOne({ where: { email: req.body.email } })
+        if (user) {
+          req.flash('error_messages', '信箱重複！')
+          return res.redirect('/signup')
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    try {
+      await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(
+          req.body.password,
+          bcrypt.genSaltSync(10),
+          null
+        ),
+      })
+      req.flash('success_messages', '成功註冊帳號！')
+      return res.redirect('/signin')
+    } catch (err) {
+      console.log(err)
+    }
+  },
+}
+
+module.exports = userController
