66da340a1a0f418be63a8169cef3be98189571ae
rayray1010 <da866810@gmail.com>
2021-11-08 17:06:34 +0800

    feat: Add restaurant index page

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const exphbs = require('express-handlebars')
+
+app.engine('hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }))
+app.set('view engine', 'hbs')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
 require('./routes')(app)
 
 module.exports = app
