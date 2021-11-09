0ee0b5017420b729f9b127a97c2e858f6bac65f5
rayray1010 <da866810@gmail.com>
2021-11-09 17:12:18 +0800

    feat:Add image to restaurants

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,6 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
+const fs = require('fs')
 
 const adminController = {
   getRestaurants: async (req, res) => {
@@ -15,15 +16,35 @@ const adminController = {
       req.flash('error_messages', "name didn't exist")
       return res.redirect('back')
     }
-    await Restaurant.create({
-      name: req.body.name,
-      tel: req.body.tel,
-      address: req.body.address,
-      opening_hours: req.body.opening_hours,
-      description: req.body.description,
-    })
-    req.flash('success_messages', 'restaurant was successfully created')
-    res.redirect('/admin/restaurants')
+    const { file } = req
+    if (file) {
+      fs.readFile(file.path, async (err, data) => {
+        if (err) console.log('Error: ', err)
+        fs.writeFile(`upload/${file.originalname}`, data, async () => {
+          await Restaurant.create({
+            name: req.body.name,
+            tel: req.body.tel,
+            address: req.body.address,
+            opening_hours: req.body.opening_hours,
+            description: req.body.description,
+            image: file ? `/upload/${file.originalname}` : null,
+          })
+          req.flash('success_messages', 'restaurant was successfully created')
+          return res.redirect('/admin/restaurants')
+        })
+      })
+    } else {
+      await Restaurant.create({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description,
+        image: null,
+      })
+      req.flash('success_messages', 'restaurant was successfully created')
+      return res.redirect('/admin/restaurants')
+    }
   },
   getRestaurant: async (req, res) => {
     const restaurant = await Restaurant.findByPk(req.params.id, { raw: true })
@@ -39,17 +60,37 @@ const adminController = {
       req.flash('error_messages', "name didn't exist")
       return res.redirect('back')
     }
-    const restaurant = await Restaurant.findByPk(req.params.id)
-    Restaurant.findByPk(req.params.id)
-    await restaurant.update({
-      name: req.body.name,
-      tel: req.body.tel,
-      address: req.body.address,
-      opening_hours: req.body.opening_hours,
-      description: req.body.description,
-    })
-    req.flash('success_messages', 'restaurant was successfully to update')
-    res.redirect('/admin/restaurants')
+    const { file } = req
+    if (file) {
+      fs.readFile(file.path, async (err, data) => {
+        if (err) console.log('Error: ', err)
+        fs.writeFile(`upload/${file.originalname}`, data, async () => {
+          const restaurant = await Restaurant.findByPk(req.params.id)
+          await restaurant.update({
+            name: req.body.name,
+            tel: req.body.tel,
+            address: req.body.address,
+            opening_hours: req.body.opening_hours,
+            description: req.body.description,
+            image: file ? `/upload/${file.originalname}` : restaurant.image,
+          })
+          req.flash('success_messages', 'restaurant was successfully to update')
+          res.redirect('/admin/restaurants')
+        })
+      })
+    } else {
+      const restaurant = await Restaurant.findByPk(req.params.id)
+      await restaurant.update({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description,
+        image: restaurant.image,
+      })
+      req.flash('success_messages', 'restaurant was successfully to update')
+      res.redirect('/admin/restaurants')
+    }
   },
   deleteRestaurant: async (req, res) => {
     const restaurant = await Restaurant.findByPk(req.params.id)
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
-
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
 module.exports = (app, passport) => {
   const authenticated = (req, res, next) => {
     if (req.isAuthenticated()) {
@@ -38,6 +39,7 @@ module.exports = (app, passport) => {
   app.post(
     '/admin/restaurants',
     authenticatedAdmin,
+    upload.single('image'),
     adminController.postRestaurant
   )
   app.get(
@@ -53,6 +55,7 @@ module.exports = (app, passport) => {
   app.put(
     '/admin/restaurants/:id',
     authenticatedAdmin,
+    upload.single('image'),
     adminController.putRestaurant
   )
   app.delete(
