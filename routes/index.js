7ebdcf575ad30b20597f08da41e017c6869c6b43
yourname <tree12132002@hotmail.com>
2021-11-09 16:15:08 +0800

    Add restaurant index page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
+const restController = require('../controllers/restController')
+
 module.exports = (app) => {
 
+  // 如果使用者訪問首頁，就導向 /restaurants的頁面
   app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.redirect('/restaurants')
   })
+  // 在 /restaurants 底下則交給 restController.getRestaurants來處理
+  app.get('/restaurants', restController.getRestaurants)
 }
