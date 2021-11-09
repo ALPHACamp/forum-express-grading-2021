a6b57aa7a862c6375eab8e9946339143c4e9404a
rayray1010 <da866810@gmail.com>
2021-11-09 14:58:19 +0800

    feat:Add authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,33 @@ const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
 
 module.exports = (app, passport) => {
-  app.get('/', (req, res) => res.redirect('/restaurants'))
-  app.get('/restaurants', restController.getRestaurant)
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) {
+        return next()
+      }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
 
-  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
+  app.get('/restaurants', authenticated, restController.getRestaurant)
+
+  app.get('/admin', authenticatedAdmin, (req, res) =>
+    res.redirect('/admin/restaurants')
+  )
+  app.get(
+    '/admin/restaurants',
+    authenticatedAdmin,
+    adminController.getRestaurants
+  )
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
