a9ffcfdc237a35cb9bf9be7964edf9e25367be80
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-10 04:24:36 +0800

    feat: add userController and sinup template with route setting

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const port = 3000;
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
