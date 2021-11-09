2c733d4be71f0479d840baf6bfe57396ebbe4a89
PaynePew <42099584+PaynePew@users.noreply.github.com>
2021-11-10 04:05:47 +0800

    chore: create db user model and migration

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const db = require("./models");
 const app = express();
 const port = 3000;
 
-app.engine("handlebars", handlebars({defaultLayout:'main'}));
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.listen(port, () => {
