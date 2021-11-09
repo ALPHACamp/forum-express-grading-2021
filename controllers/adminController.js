ecce7bfc59b6ef63a729678dcebad611d7d9cbaf
rayray1010 <da866810@gmail.com>
2021-11-09 15:41:41 +0800

    feat:Add admin restaurant post feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,6 +6,25 @@ const adminController = {
     const restaurants = await Restaurant.findAll({ raw: true })
     return res.render('admin/restaurants', { restaurants })
   },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create')
+  },
+
+  postRestaurant: async (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    }
+    await Restaurant.create({
+      name: req.body.name,
+      tel: req.body.tel,
+      address: req.body.address,
+      opening_hours: req.body.opening_hours,
+      description: req.body.description,
+    })
+    req.flash('success_messages', 'restaurant was successfully created')
+    res.redirect('/admin/restaurants')
+  },
 }
 
 module.exports = adminController
