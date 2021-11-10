5b2b211b7ecb77ea88b9e3d52e731bfaf56889bc
Liangni <light0104@gmail.com>
2021-11-10 20:07:52 +0800

    Add isAdmin to users table and Add authentication before rendering pages

---
+++ migrations/20211110111430-add-isAdmin-to-users.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const { sequelize } = require("../models");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Users', 'isAdim', {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Users', 'isAdmin')
+  }
+};
---
+++ migrations/20211110112518-remove-isAdim-from-users.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Users', 'isAdim')
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Users', 'isAdim', {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    })
+  }
+};
