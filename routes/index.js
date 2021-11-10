bd6e7e216aa78e4355e742a41df013017eb15fb3
yourname <tree12132002@hotmail.com>
2021-11-11 06:28:44 +0800

    Add admin restaurant index page

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ module.exports = (app, passport) => {
   }
 
   // 如果使用者訪問首頁，就導向 /restaurants的頁面
-  app.get('/', authenticated,(req, res) => res.redirect('/restaurants'))
+  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
   // 在 /restaurants 底下則交給 restController.getRestaurants來處理
   app.get('/restaurants', authenticated, restController.getRestaurants)
 
