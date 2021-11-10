27429e7643d4ad0fed21ab18eeeed85d3282d212
Tim <s87462830@gmail.com>
2021-11-10 22:52:24 +0800

    feat: Add restaurant index page

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const exhbs = require('express-handlebars')
+
 const app = express()
 const port = 3000
+
+// 設定 view engine 使用 handlebars
+app.engine('hbs', exhbs({
+  defaultLayout: 'main',
+  extname: 'hbs',
+}))
+app.set('view engine', 'hbs')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
 require('./routes')(app)
 
 module.exports = app
