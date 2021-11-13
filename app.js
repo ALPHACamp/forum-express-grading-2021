d892c4274ffdee21b0767d9fd168c7ecf060eb65
tsaohs <steventsao2012@gmail.com>
2021-11-13 15:55:52 -0800

    Install pg and update production config

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
+const methodOverride = require('method-override')
+const db = require('./models') // 引入資料庫
+const flash = require('connect-flash')
+const session = require('express-session')
+const passport = require('./config/passport')
 const app = express()
 const port = 3000
+
+// 設定 view engine 使用 handlebars
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+app.use('/upload', express.static(__dirname + '/upload'))
+
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+
+app.use(passport.initialize())
+app.use(passport.session())  
+
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  console.log(req.user)
+  res.locals.user = req.user
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
+// require('./routes')(app)
 
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,48 @@
-module.exports = (app) => {
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+const restController = require('../controllers/restController.js')
+const adminController = require('../controllers/adminController.js') // 加入這行
+const userController = require('../controllers/userController.js')
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
+
+
+
+module.exports = (app, passport) => {
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { 
+        return next() 
+      }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }  
+
+  //如果使用者訪問首頁，就導向 /restaurants 的頁面
+  app.get('/', (req, res) => res.redirect('/restaurants'))
+  //在 /restaurants 底下則交給 restController.getRestaurants 來處理
+  app.get('/restaurants', authenticated, restController.getRestaurants)
+
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
+  app.get('/signin', userController.signInPage)
+  app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), 
+                    userController.signIn)
+  app.get('/logout', userController.logout)
+
+  // 連到 /admin 頁面就轉到 /admin/restaurants
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+  // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
+  app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
+  app.post('/admin/restaurants', authenticatedAdmin, upload.single('image'), adminController.postRestaurant)
+  app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
+  app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
+  app.put('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
+  app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
 }
