595fbe2ac68a94dca64bd1cdba14e724f842e098
Lily <liiy120530@gmail.com>
2021-11-13 12:00:29 +0800

    Install multer and add image upload feature

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
 
-
 const app = express()
 const port = 3000
 
@@ -18,6 +17,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
 app.use(methodOverride('_method'))
+app.use('/upload', express.static(__dirname + '/upload'))
 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
