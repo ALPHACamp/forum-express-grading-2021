95a6824b55f2242e4596d7cfba63436e2e4936f1
william1993314 <william1993314@gmail.com>
2021-11-14 19:29:47 +0800

    Confirm user password and unique email

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-const express = require('express');
-const handlebars  = require('express-handlebars');
+const express = require('express')
+const handlebars  = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
 app.use(express.urlencoded({extended: true}))
 
-const hbs = handlebars.create({ /* config */ });
-app.engine('handlebars', hbs.engine);
+const hbs = handlebars.create({ /* config */ })
+app.engine('handlebars', hbs.engine)
 // app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 
-app.set('view engine', 'handlebars');
+app.set('view engine', 'handlebars')
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
 
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
