a5d5e2f3c9c44a6cba4a6a007cddb9105809bc94
william1993314 <william1993314@gmail.com>
2021-11-14 19:04:56 +0800

    Initialize sequelize and connect to database

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
-// const express = require('express')
-// const handlebars = require('express-handlebars')
-// const app = express()
-// const port = 3000
-//
-// app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
-// app.set('view engine', 'handlebars')
-//
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}!`)
-// })
-//
-// require('./routes')(app)
-//
-// module.exports = app
-
-
-
 const express = require('express');
 const handlebars  = require('express-handlebars');
+const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
-const hbs = handlebars.create({ /* config */ });
 
-// Register `hbs.engine` with the Express app.
+const hbs = handlebars.create({ /* config */ });
 app.engine('handlebars', hbs.engine);
+// app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+
 app.set('view engine', 'handlebars');
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
@@ -32,3 +17,6 @@ app.listen(port, () => {
 require('./routes')(app)
 
 module.exports = app
+
+
+
