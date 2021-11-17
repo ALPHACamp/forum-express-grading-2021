10decead41b0690efe31e1382e01d02cb4ef9700
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-11-17 17:19:57 +0800

    Convert time format using moment

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -1,9 +1,15 @@
+const moment = require('moment')
+
 module.exports = {
-  ifCond: function(a, b, options) {
+  ifCond: function (a, b, options) {
     if (a === b) {
       return options.fn(this)
     } else {
       return options.inverse(this)
     }
+  },
+
+  moment: function (a) {
+    return moment(a).fromNow()
   }
 }
