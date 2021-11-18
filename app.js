4e00a21101b2a679fb17d55f71d01b4ddee3cc1d
ohohopen <ohohopen@gmail.com>
2021-11-19 07:48:42 +0800

    Initialize sequelize and connect to database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars"); // 引入 handlebars
 const app = express();
 const port = 3000;
-
+const db = require("./models"); // 引入資料庫
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" })); // Handlebars 註冊樣板引擎
 app.set("view engine", "hbs"); // 設定使用 Handlebars 做為樣板引擎
 
