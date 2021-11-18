d26a398a3a916fd12abe193468c9a1399be4c91c
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-11-18 23:12:50 +0800

    feat:Add userController and signup page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(express.urlencoded({extended: true}))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
