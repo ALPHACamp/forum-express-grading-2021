b4fb009003c74d218fc5029b26ff224a4d4523aa
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-11-19 23:54:39 +0800

    Listen to process.env.PORT & define Procfile

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const methodOverride = require('method-override')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
