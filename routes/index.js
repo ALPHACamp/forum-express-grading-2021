9681de5829a25a4dbf313332a294bdcf47ce4902
ohohopen <ohohopen@gmail.com>
2021-11-20 09:38:07 +0800

    Add userController and signup page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const restController = require("../controllers/restController.js");
 const adminController = require("../controllers/adminController.js"); // 加入這行
+const userController = require("../controllers/userController");
 
 module.exports = (app) => {
 	app.get("/", (req, res) => res.redirect("/restaurants"));
@@ -10,4 +11,7 @@ module.exports = (app) => {
 
 	// 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
 	app.get("/admin/restaurants", adminController.getRestaurants);
+
+	app.get("/signup", userController.signUpPage);
+	app.post("/signup", userController.signUp);
 };
