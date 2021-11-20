4a0d921d3fe42929a267d8cda27e6aac30bda3db
ohohopen <ohohopen@gmail.com>
2021-11-20 17:15:30 +0800

    Add feature: signInPage, signIn and logout

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const exphbs = require("express-handlebars"); // 引入 handlebars
 const app = express();
 const port = 3000;
 const db = require("./models"); // 引入資料庫
+const passport = require("./config/passport");
+
+//
 const flash = require("connect-flash");
 const session = require("express-session");
 app.use(flash());
@@ -21,11 +24,12 @@ app.use((req, res, next) => {
 app.use(express.urlencoded({ extended: true }));
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" })); // Handlebars 註冊樣板引擎
 app.set("view engine", "hbs"); // 設定使用 Handlebars 做為樣板引擎
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}!`);
 });
 
-require("./routes")(app);
+require("./routes")(app, passport);
 
 module.exports = app;
