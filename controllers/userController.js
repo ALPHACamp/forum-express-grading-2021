db83e6b32f00b31d2b89c0ae7c2f761c4903a3e1
Liangni <light0104@gmail.com>
2021-11-21 15:53:42 +0800

    Add get user profile & edit user profile feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,10 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
+const fs = require('fs')
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+const helpers = require('../_helpers')
 
 const userController = {
   signUpPage: (req, res) => {
@@ -42,6 +46,60 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+
+  getUser: (req, res) => {
+    return User.findByPk(req.params.id)
+      .then((user) => {
+        const loginUser = helpers.getUser(req)
+        return res.render('profile', { user: user.toJSON(), loginUser: loginUser })
+      })
+  },
+  editUser: (req, res) => {
+    if (helpers.getUser(req).id !== Number(req.params.id)) {
+      req.flash('error_messages', '你沒有訪問此頁面的權限')
+      return res.redirect(`/users/${helpers.getUser(req).id}`)
+    }
+    return User.findByPk(req.params.id)
+      .then((user) => {
+        return res.render('edit', { user: user.toJSON() })
+      })
+  },
+  putUser: (req, res) => {
+    
+    if (!req.body.name) {
+      req.flash('error_messages', '請輸入使用者名稱')
+      return res.redirect('back')
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        return User.findByPk(req.params.id)
+          .then((user) => {
+            return user.update({
+              name: req.body.name,
+              email: req.body.email,
+              image: img ? img.data.link : user.image
+            }).then((user) => {
+              req.flash('success_messages', '使用者資料編輯成功')
+              return res.redirect(`/users/${req.params.id}`)
+            })
+          })
+      })
+    } else {
+      return User.findByPk(req.params.id)
+      .then((user) => {
+        return user.update({
+          name: req.body.name,
+          email: req.body.email,
+        }).then((user) => {
+          req.flash('success_messages', '使用者資料編輯成功')
+          return res.redirect(`/users/${req.params.id}`)
+        })
+      })   
+    }
   }
 }
 
