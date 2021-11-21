8e70a554e99e28c24cb69e8db2a63ff515d8f6fe
leo812leo <leo_812leo@hotmail.com.tw>
2021-11-21 22:05:24 +0800

    _helpers

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
-<<<<<<< HEAD
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
+const helpers = require('../_helpers')
 
 module.exports = (app, passport) => {
   const authenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
+    if (helpers.ensureAuthenticated(req)) {
       return next()
     }
     res.redirect('/signin')
   }
   const authenticatedAdmin = (req, res, next) => {
-    if (req.isAuthenticated()) {
+    if (helpers.ensureAuthenticated(req)) {
       if (req.user.isAdmin) { return next() }
       return res.redirect('/')
     }
@@ -41,10 +41,3 @@ module.exports = (app, passport) => {
   app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
   app.get('/logout', userController.logout)
 }
-=======
-module.exports = (app) => {
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-}
->>>>>>> origin/R01-test
