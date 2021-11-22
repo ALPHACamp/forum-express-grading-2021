63ac2eaf788b6fa594fdc6bf6a3fbfac2b3749de
shibao <dream0184@yahoo.com.tw>
2021-11-22 21:23:13 +0800

    feat: add isAdmin column to users, create admin routes and admin login function

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,27 @@ const adminController = require('../controllers/adminController.js')
 const restController = require('../controllers/restController.js')
 const userController = require('../controllers/userController.js')
 module.exports = (app, passport) => {
-  app.get('/', (req, res) => { res.redirect('/restaurants') })
-  app.get('/restaurants', restController.getRestaurant)
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) {
+        return next()
+      }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
 
-  app.get('/admin', (req, res) => res.redirect('/admon/restaurants'))
-  app.get('/admin/restaurants', adminController.getRestaurant)
+  app.get('/', authenticated, (req, res) => { res.redirect('/restaurants') })
+  app.get('/restaurants', authenticated, restController.getRestaurant)
+
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurant)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
