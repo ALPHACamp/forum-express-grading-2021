7c9211d1c72d000b18d7994496764f99c10176e2
shibao <dream0184@yahoo.com.tw>
2021-11-22 16:18:21 +0800

    feat: add admin, restaurant routes and controllers

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
+const adminController = require('../controllers/adminController.js')
+const restController = require('../controllers/restController.js')
 module.exports = (app) => {
+  app.get('/', (req, res) => { res.redirect('/restaurants') })
+  app.get('/restaurants', restController.getRestaurant)
 
-  app.get('/', (req, res) => {
-    res.render('index')
-  })
+  app.get('/admin', (req, res) => res.redirect('/admon/restaurants'))
+  app.get('/admin/restaurants', adminController.getRestaurant)
 }
