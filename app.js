edf035a8c19f60d0a2236d185c6e429f619d2dbb
shibao <dream0184@yahoo.com.tw>
2021-11-24 00:11:08 +0800

    feat: install imgur-node-api and dotenv. Add image upload function

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
