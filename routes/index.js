1e2cc274779b91cd5949533eee8ef84a5eee9dfb
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-11-23 23:21:25 +0800

    feat:Add restaurants show page

--- routes/index.js
+++ routes/index.js
@@ -23,6 +23,8 @@ module.exports = (app, passport) => {
   //瀏覽首頁
   app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', authenticated,restController.getRestaurants)
+  //使用者瀏覽畫面
+  app.get('/restaurants/:id', authenticated, restController.getRestaurant)
   //瀏覽後台
   app.get('/admin', authenticatedAdmin,(req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
