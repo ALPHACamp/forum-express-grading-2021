cb4cc7480bc0c77d0bf6f34d09c66d7166baf391
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-11-23 11:40:20 +0800

    feat:Add admin categories page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
+const categoryController = require('../controllers/categoryController.js')
 const helpers = require('../_helpers')
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
@@ -45,4 +46,6 @@ module.exports = (app, passport) => {
   app.get('/signin', userController.signInPage)
   app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
   app.get('/logout', userController.logout)
+  //餐廳分類
+  app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
 }
