13384e261bdd2131fee87c973c07373fb9ced8c0
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-11-23 10:59:45 +0800

    feat:Show category name

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,10 +4,15 @@ const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const db = require('../models') 
 const Restaurant = db.Restaurant
 const User = db.User
+const Category = db.Category
 
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({raw: true, nest: true})
+    return Restaurant.findAll({
+      raw: true,
+      nest: true, 
+      include: [Category]
+    })
     .then(restaurants => {
       return res.render('admin/restaurants', { restaurants })
     })
@@ -56,9 +61,11 @@ const adminController = {
   },
 
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, {raw:true})
+    return Restaurant.findByPk(req.params.id, {
+      include: [Category]
+    })
     .then(restaurant => {
-      return res.render('admin/restaurant', { restaurant }) 
+      return res.render('admin/restaurant', { restaurant: restaurant.toJSON() }) 
     })
   },
 
