008cacb17e04d5d3d24228e2d3cf87ca81e5550e
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-11-24 23:27:50 +0800

    feat:Show related comments on restaurant page

--- helpers/handlebarsHelpers.js
+++ helpers/handlebarsHelpers.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+
 module.exports = {
     showRole: function (isAdmin) {
         if (isAdmin) return 'admin'
@@ -12,5 +14,8 @@ module.exports = {
           return options.fn(this)
           }
         return options.inverse(this)
+    },
+    moment: function (a) {
+        return moment(a).fromNow()
     }
 }
