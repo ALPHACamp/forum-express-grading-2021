d250cd75e9d653b7283493299308f26a268a91bd
ohohopen <ohohopen@gmail.com>
2021-11-27 09:49:54 +0800

    add Profile and setting port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars"); // 引入 handlebars
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const db = require("./models"); // 引入資料庫
 const passport = require("./config/passport");
 const methodOverride = require("method-override");
