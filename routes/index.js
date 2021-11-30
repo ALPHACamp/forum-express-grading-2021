f762c64119d29d55dcdd539e200d5da095685979
Kemal-Wuzhi <camelwuzhi@gmail.com>
2021-11-30 22:40:17 +0800

    fix several bugs

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ module.exports = (app, passport) => {
     res.redirect('/signin')
   }
 
-  //如果使用者訪問首頁，就導向 /restaurants.hbs 的頁面
+  //如果使用者訪問首頁，就導向 /restaurants.hbs 
   app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
   //在 /restaurants 底下則交給 restController.getRestaurants 這個function來處理
   app.get('/restaurants', authenticated, restController.getRestaurants)
