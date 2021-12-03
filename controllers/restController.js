06fee9358447aba57ac5af20ef08f6370bf05cc8
Kemal-Wuzhi <camelwuzhi@gmail.com>
2021-12-03 14:55:40 +0800

    create comment model & add association among users,comment,restaurants

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+
+const pageLimit = 10
 //在這個例子裡，restController 是 controller 的名稱，
 //而 getRestaurants 則是 controller 裡的一個 action。
 //如果把程序抽取到 controller 裡，
@@ -9,29 +11,49 @@ const restController = {
   //getRestaurants 這個屬性的值是一個 function，而這個 function 負責「瀏覽餐廳頁面」
   //，也就是去 render 一個叫做 restaurants 的樣板
   getRestaurants: (req, res) => {
+    let offset = 0
     const whereQuery = {}
     let categoryId = ''
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
-      whereQuery.CategoryId = categoryId
+      whereQuery.categoryId = categoryId
     }
-    Restaurant.findAll(({
+    Restaurant.findAndCountAll({
       include: Category,
-      where: whereQuery
-    })).then(restaurants => {
-      const data = restaurants.map(r => ({
+      where: whereQuery,
+      offset: offset,
+      limit: pageLimit
+    }).then(result => {
+      // data for pagination
+      const page = Number(req.query.page) || 1
+      const pages = Math.ceil(result.count / pageLimit)
+      const totalPage = Array.from({
+        length: pages
+      }).map((item, index) => index + 1)
+      const prev = page - 1 < 1 ? 1 : page - 1
+      const next = page + 1 > pages ? pages : page + 1
+
+      // clean up restaurant data
+      const data = result.rows.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),
-        categoryName: r.Category.name
+        categoryName: r.dataValues.Category.name
       }))
       Category.findAll({
         raw: true,
         nest: true
-      }).then(categories => { // 取出 categoies
+      }).then(categories => {
         return res.render('restaurants', {
           restaurants: data,
           categories: categories,
-          categoryId: categoryId
+          categoryId: categoryId,
+          page: page,
+          totalPage: totalPage,
+          prev: prev,
+          next: next
         })
       })
     })
