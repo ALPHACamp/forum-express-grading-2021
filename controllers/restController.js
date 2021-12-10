963c6735eea07cb5d79f1b11f9dd59aa7ceb3ad1
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-12-10 11:24:16 +0800

    feat:Switch add/remove favorite button on restaurants index page and single page

--- controllers/restController.js
+++ controllers/restController.js
@@ -35,7 +35,8 @@ const restController = {
       const data = result.rows.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),
-        categoryName: r.dataValues.Category.name
+        categoryName: r.dataValues.Category.name,
+        isFavorited: req.user.FavoritedRestaurants.map(d => d.id).includes(r.id)
       }))
       Category.findAll({
         raw: true,
@@ -57,11 +58,18 @@ const restController = {
     return Restaurant.findByPk(req.params.id, {
       include: [
         Category,
+        { model: User, as: 'FavoritedUsers' },
         { model: Comment, include: [User] }
       ]
-    }).then(restaurant => 
-      restaurant.increment({viewCounts: 1})
-      .then(() =>res.render('restaurant', { restaurant: restaurant.toJSON() }))
+    }).then(restaurant =>{
+      const isFavorited = restaurant.FavoritedUsers.map(d => d.id).includes(req.user.id);
+      return restaurant.increment({viewCounts: 1})
+      .then(() =>res.render('restaurant', { 
+        restaurant: restaurant.toJSON(),
+        isFavorited
+      }))
+    } 
+     
     )
   },
   getFeeds: (req, res) => {
