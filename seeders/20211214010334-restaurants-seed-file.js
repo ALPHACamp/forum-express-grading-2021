a8738860680c8f4417fc8ca5b486328bc4e1eda6
ohohopen <ohohopen@gmail.com>
2021-12-14 09:22:24 +0800

    Add category seed data

--- seeders/20211214010334-restaurants-seed-file.js
+++ seeders/20211214010334-restaurants-seed-file.js
@@ -15,6 +15,7 @@ module.exports = {
 				description: faker.lorem.text(),
 				createdAt: new Date(),
 				updatedAt: new Date(),
+				CategoryId: Math.floor(Math.random() * 6) * 10 + 1,
 			})),
 			{}
 		);
