8ca6faa69de0a13f64c5e75937c7254f509ae4ec
Richard Widjaya <ricwidjaya@gmail.com>
2021-12-28 20:30:09 +0800

    feat: add admin restaurant view

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,6 +1,11 @@
+const db = require("../models")
+const Restaurant = db.Restaurant
+
 const adminController = {
   getRestaurants: (req, res) => {
-    return res.render("admin/restaurants")
+    Restaurant.findAll({ raw: true, nest: true }).then((restaurants) => {
+      return res.render("admin/restaurants", { restaurants })
+    })
   }
 }
 
