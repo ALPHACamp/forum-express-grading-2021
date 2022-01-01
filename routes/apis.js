8ce9db615fb029ea82a37fba196705e287759606
HUANG-SIH-MAN <cindy20303705@gmail.com>
2022-01-01 20:21:39 +0800

    feat:POST /api/admin/restaurants

--- routes/apis.js
+++ routes/apis.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../controllers/api/adminController.js')
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
 
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.get('/admin/categories', adminController.getCategories)
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
+router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
 
 module.exports = router
