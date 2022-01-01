7bd94f8bb48af536a3d32b09668ead3e5bea0ee1
HUANG-SIH-MAN <cindy20303705@gmail.com>
2022-01-01 22:54:55 +0800

    feat:PUT /api/admin/categories/:id - 後台修改類別

--- services/adminService.js
+++ services/adminService.js
@@ -123,6 +123,19 @@ const adminService = {
                 callback({ status :'success', message: ''})
             })
         }
+    },
+    putCategory: (req, res, callback) => {
+        if (!req.body.name) {
+            callback({ status :'error', message: 'name didn\'t exist'})
+        } else {
+          return Category.findByPk(req.params.id)
+            .then((category) => {
+              category.update(req.body)
+                .then((category) => {
+                    callback({ status :'success', message: ''})
+                })
+            })
+        }
     } 
 }
 
