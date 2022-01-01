c1238f75cf4b1e71f1192b9c9fe41df37111df53
HUANG-SIH-MAN <cindy20303705@gmail.com>
2022-01-01 23:04:20 +0800

    feat:DELETE /api/admin/categories/:id - 後台刪除類別

--- services/adminService.js
+++ services/adminService.js
@@ -136,7 +136,16 @@ const adminService = {
                 })
             })
         }
-    } 
+    },
+    deleteCategory: (req, res, callback) => {
+        return Category.findByPk(req.params.id)
+          .then((category) => {
+            category.destroy()
+              .then((category) => {
+                callback({ status :'success', message: ''})
+              })
+          })
+      }
 }
 
 module.exports = adminService
