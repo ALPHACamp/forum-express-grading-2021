3a143aec8ff8d0568b006b5bdabf08cd0eb201fc
DESKTOP-VBC15AG\Administrator <catcolor2020@gmail.com>
2022-01-11 02:37:03 +0800

    add seed comment

--- seeders/20211223170601-users-seed-file.js
+++ seeders/20211223170601-users-seed-file.js
@@ -4,24 +4,30 @@ const bcrypt = require('bcryptjs')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Users', [{
+      id: 1,
       email: 'root@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: true,
       name: 'root',
+      image: "https://images.unsplash.com/photo-1558024920-b41e1887dc32?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
+      id: 2,
       email: 'user1@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
       name: 'user1',
+      image: "https://images.unsplash.com/photo-1560691023-ca1f295a5173?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
+      id: 3,
       email: 'user2@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
       name: 'user2',
+      image: "https://images.unsplash.com/photo-1568271675068-f76a83a1e2d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
       createdAt: new Date(),
       updatedAt: new Date()
     }], {})
