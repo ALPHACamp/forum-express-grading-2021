d2cdbad6e5d4ff2ed2f3d4cf76e3255857ce63ed
DESKTOP-VBC15AG\Administrator <catcolor2020@gmail.com>
2022-01-11 21:49:45 +0800

    seeders restaurants seed comment seed

--- seeders/20220110174037-comments-seed-file.js
+++ seeders/20220110174037-comments-seed-file.js
@@ -6,10 +6,9 @@ module.exports = {
     await queryInterface.bulkInsert('Comments',
       Array.from({ length: 10 }).map((d, i) =>
       ({
-        id: i + 1,
         text: faker.lorem.text().substring(0, 50),
         UserId: Math.floor(Math.random() * 3) + 1,
-        RestaurantId: Math.floor(Math.random() * 50) + 1,
+        RestaurantId: Math.floor(Math.random() * 307) + 1,
         createdAt: new Date(),
         updatedAt: new Date()
       })
